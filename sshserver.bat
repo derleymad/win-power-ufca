6fa4be7a5ecab485d14653852467ca021e2a86b9
derleymad <filho.wanderley@hotmail.com>
2022-09-03 10:29:53 -0300

    adding chocolatey instalation

--- sshserver.bat
+++ sshserver.bat
@@ -1,3 +1,4 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install openssh -y -f
 ssh -Vssh -V
 cd "C:\Program Files\OpenSSH-Win64"
