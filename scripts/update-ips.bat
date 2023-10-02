276e6e98f6cadc20a33bfd423988a3a084abebed
caioricardo42 <120578133+caioricardo42@users.noreply.github.com>
2023-10-02 15:12:01 -0300

    Inserção das planilhas dos labs do CCAB o script

--- scripts/update-ips.bat
+++ scripts/update-ips.bat
@@ -9,6 +9,8 @@ wget -q "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/planilh
 wget -q "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/planilhas/FILAS%%20-%%20LAB_K02.csv" -O "FILAS - LAB_K02.csv"
 wget -q "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/planilhas/FILAS%%20-%%20LAB_K04.csv" -O "FILAS - LAB_K04.csv"
 wget -q "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/planilhas/FILAS%%20-%%20LAB_K07.csv" -O "FILAS - LAB_K07.csv"
+wget -q "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/planilhas/FILAS%20-%20CCAB_M105%20.csv" - O "FILAS - CCAB_M105.csv"
+wget -q "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/planilhas/FILAS%20-%20CCAB_M107.csv" - O "FILAS - CCAB_M107.csv"
 
 curl -O "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/server-config/server-config.json"
 curl -O "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/chave/lab_public_key.pem"
@@ -22,4 +24,6 @@ curl -O "https://raw.githubusercontent.com/derleymad/win-power-ufca/main/chave/l
 "C:\Program Files\Veyon\veyon-cli.exe" networkobjects import "FILAS - LAB_K02.csv" location "LAB_K02" format "%%name%%,%%host%%,%%mac%%"
 "C:\Program Files\Veyon\veyon-cli.exe" networkobjects import "FILAS - LAB_K04.csv" location "LAB_K04" format "%%name%%,%%host%%,%%mac%%"
 "C:\Program Files\Veyon\veyon-cli.exe" networkobjects import "FILAS - LAB_K07.csv" location "LAB_K07" format "%%name%%,%%host%%,%%mac%%"
+"C:\Program Files\Veyon\veyon-cli.exe" networkobjects import "FILAS - CCAB_M105.csv" location "CCAB_M105" format "%%name%%,%%host%%,%%mac%%"
+"C:\Program Files\Veyon\veyon-cli.exe" networkobjects import "FILAS - CCAB_M107.csv" location "CCAB_M107" format "%%name%%,%%host%%,%%mac%%"
 PAUSE
